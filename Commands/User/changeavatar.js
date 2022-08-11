5990f52f91060c1d104ae65fa3765681787e08ce
phongprovip1401 <98637275+phongprovip1401@users.noreply.github.com>
2022-08-11 13:49:47 +0700

    Update changeavatar.js

--- Commands/User/changeavatar.js
+++ Commands/User/changeavatar.js
@@ -4,9 +4,9 @@ module.exports = {
   run: async function avatar (client, message, process) {
   const avatarFlag = message.content.split(' ')[1]
 
-  if (![client.user.id, '877059303746469898'].includes(message.author.id)) return message.channel.send('Chỉ Nguyễn Thiên Phong#4973 mới được quyền sử dụng lệnh này!');
+  if (![client.user.id, '877059303746469898'].includes(message.author.id)) return message.channel.send('Chỉ Owner mới được quyền sử dụng lệnh này!');
 
-  if (!avatarFlag) { return message.channel.send('debug url') }
+  if (!avatarFlag) { return message.channel.send('Null') }
 
   await client.user.setAvatar(avatarFlag)
   const allIsSet = await message.channel.send(`da doi avt`)
